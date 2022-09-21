9809535040546afe96f07cfa77ce22782037ae50
Alexsander Nesterov <alex93nest@mail.ru>
2022-09-21 12:46:46 +0600

    one

---
+++ src/test/java/com/example/hiber/HiberApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.hiber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HiberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
